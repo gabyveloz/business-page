c507d32ee7569acec0d6244647761d20731b9204
Gabriela Veloz <velozitywc@gmail.com>
2020-03-19 16:57:35 -0600

    My personal website project.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,96 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>Personal Website Project</title>
+</head>
+<body>
+
+<h2>Milestone 1</h2>
+
+<div>
+	<strong>PURPOSE:</strong>
+
+	Sell stylish fitness clothing like yoga pants and
+	sports bras. Any sports attire eventually, but to
+	keep it simple for now just bottoms and tops.
+</div>
+
+<div>
+	<p><strong>AUDIENCE:</strong>
+
+	<li>Females</li>
+	<li>Age 14+</li>
+	<li>Atheletes</li>
+	<li>Non-athelets/for comfort</li>
+	<li>Potential Customers</li>
+	</p>
+</div>
+
+<div>
+	<strong>GOAL:</strong>
+
+		To reach any potential customers wanting to purchase
+		new gym clothing that fits them great and looks cute.
+		<p>
+	   <li>Earn part time income in selling this product</li>
+		<li>Create a well known brand.</li>
+		<li>Create a demand.</li>
+		<li>Keep them at an affordable rate.</li>
+</p>
+
+</div>
+
+
+	<div>
+
+		<img src="Crystal.jpg"/>
+<p>
+		<li>Name: Crystal</li>
+		<li>Age: 35 years old</li>
+		<li>Job: Marketing/sales</li>
+		<li>Device: Apple Iphone 8</li>
+	</p>
+	</div>
+
+<div>
+	<strong>ATTITUDES/BEHAVIORS:</strong>
+
+	Likes to work out every few days. She is always ery short
+	on time because of work. She likes to be comfortable and
+	stylish at the same time in case she needs to meet with people.
+	Is not too huge on technology, prefers person to person
+	contact. Does scroll a lot through social media. Likes to
+	shop online and find great deals.
+</div>
+
+<div>
+	<p><strong>NEEDS/FRUSTRATIONS:</strong>
+	Crystal needs to order a new set of gym clothes. She likes
+	simple transactions that allow her to input her information
+	and find the correct sizes with a simple checkout. She needs
+	good quality leggings that are not see through. She is
+	frustrated the most when she cannot find the correct size and
+	cannot mix and match outfits. She does not like slow shipping
+	or needing to return an item.
+	</p>
+</div>
+
+<div>
+	<p><strong>GOALS:</strong>
+	Crystal wants to purchase a new set of leggings and sports
+	bras at a decent price. She wants them to be cute enough
+	to also wear out on a casual day. She would like fast
+	shipping and a simple way to browse and filter through
+	her options.
+	</p>
+</div>
+
+<ul><li>
+As a buyer Crystal just wants to buy a new gym outfit!
+	</li></ul>
+
+</body>
+
+</html>
